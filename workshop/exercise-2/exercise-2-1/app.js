ddd06d6007e1ca726ba71d4e122a4299d3999c6f
Caroline Vinet <carovinet@gmail.com>
2020-06-17 17:49:41 -0400

    exercise2

--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -1,22 +1,30 @@
-// Preset values
 const FROGS = 3;
-const orderedList = document.getElementById("track");
-
-for (i = 1; i <= FROGS; i++) {
+const track = document.getElementById("track");
+let progressContainer = document.createElement("div");
+const main = document.querySelector("main");
+main.appendChild(progressContainer);
+for (let i = 0; i < FROGS; i++) {
   let lane = document.createElement("li");
-  orderedList.appendChild(lane);
-
-  let laneNumber = document.createElement("span");
-  laneNumber.innerText = i;
-  lane.appendChild(laneNumber);
-
-  lane.id = "frog " + i;
+  track.appendChild(lane);
+  let number = document.createElement("span");
+  number.innerText = i + 1;
+  lane.appendChild(number);
+  lane.id = "lane-" + i;
 }
-
 const racers = [];
-
 for (let x = 0; x < FROGS; x++) {
   racers.push(frogStable[x]);
 }
-
-console.log(racers);
+racers.forEach(function (racer, index) {
+  let lane = document.getElementById("lane-" + index);
+  let frog = document.createElement("span");
+  frog.classList.add("frog");
+  frog.style.backgroundColor = racer.color;
+  frog.innerText = "frog" + index;
+  frog.progress = 0;
+  let progressSpan = document.createElement("span");
+  progressSpan.id = "lane-progress-" + index;
+  progressSpan.innerText = frog.progress + "%";
+  progressContainer.appendChild(progressSpan);
+  lane.appendChild(frog);
+});
